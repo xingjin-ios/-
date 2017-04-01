549e30046e3713e9cb1ed08295d1fe1763159479
邢进 <xingjin@xingjindeMacBook-Pro.local>
2017-04-01 22:04:59 +0800

    Initial Commit

---
+++ KeyWordRearrangeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyWordRearrangeDemo
+//
+//  Created by 邢进 on 2017/4/1.
+//  Copyright © 2017年 邢进. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
